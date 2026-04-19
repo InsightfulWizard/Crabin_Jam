8ce00788124fde263ddc09be35eaa679b859c168
jennymaeleidig <jennymaeleidig@gmail.com>
2026-04-19 00:27:05 -0400

    attempt patch

--- scripts/crab.gd
+++ scripts/crab.gd
@@ -1,11 +1,13 @@
 extends Node2D
+
 @onready var sweat: CPUParticles2D = $sweat
 
 
 func _ready() -> void:
 	GameState.connect('score_changed', _on_score_changed)
 
-func _on_score_changed():
+
+func _on_score_changed(_score: int):
 	if GameState.state == GameState.CHILLIN:
 		toggle_chillin(true)
 		toggle_stresed(false)
@@ -18,13 +20,15 @@ func _on_score_changed():
 		toggle_chillin(false)
 		toggle_stresed(false)
 		toggle_dread(true)
-		
 
-func toggle_chillin(b:bool):
+
+func toggle_chillin(b: bool):
 	pass
 
-func toggle_stresed(b:bool):
+
+func toggle_stresed(b: bool):
 	pass
 
-func toggle_dread(b:bool):
+
+func toggle_dread(b: bool):
 	sweat.emitting = b
