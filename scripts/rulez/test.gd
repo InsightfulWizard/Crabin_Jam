fec4cc22305922a4d498cf44cb2e1a8ba3ff238d
InsightfulWizard <57632526+InsightfulWizard@users.noreply.github.com>
2026-04-22 21:11:22 -0400

    Extra cool shades

--- scripts/rulez/test.gd
+++ scripts/rulez/test.gd
@@ -1,16 +1,16 @@
 extends SceneTree
 
 func _init():
-    print("--- Logic Test Start ---")
-    
-    # Call your utility functions here
-    var result = my_test_logic()
-    print("Result: ", result)
-    
-    print("--- Logic Test End ---")
-    
-    # Important: Tell the process to stop
-    quit()
+	print("--- Logic Test Start ---")
+	
+	# Call your utility functions here
+	var result = my_test_logic()
+	print("Result: ", result)
+	
+	print("--- Logic Test End ---")
+	
+	# Important: Tell the process to stop
+	quit()
 
 func my_test_logic():
-    return 1 + 1
+	return 1 + 1
